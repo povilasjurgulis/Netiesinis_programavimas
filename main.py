d04e9fe0a4ff329a93e7f816a7b810b25f2e6a5a
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2026-04-28 22:39:09 +0300

    Padarytas funkcijos reikšmių apskaičiavimas ir realizuota baudos funkcija

--- main.py
+++ main.py
@@ -26,3 +26,34 @@ def h(X):
     h2 = -y  # -y <= 0, t.y. y >= 0
     h3 = -z  # -z <= 0, t.y. z >= 0
     return np.array([h1, h2, h3])
+
+
+# ---------------------------------
+# 2. Funkcijų reikšmių apskaičiavimas
+
+# Taške X0 funkcijos reikšmės
+X0 = [0, 0, 0]
+print (f"f(X0) = {f(X0)}, g(X0) = {g(X0)}, h(X0) = {h(X0)}")
+
+# Taške X1 funkcijos reikšmės
+X1 = [1, 1, 1]
+print (f"f(X1) = {f(X1)}, g(X1) = {g(X1)}, h(X1) = {h(X1)}")
+
+# Taške Xm funkcijos reikšmės
+Xm = [9/10, 3/10, 4/10]
+print (f"f(Xm) = {f(Xm)}, g(Xm) = {g(Xm)}, h(Xm) = {h(Xm)}")
+
+
+# ------------------
+# 3. Baudos funkcija
+
+def b(X):
+    return max(0, g(X))**2 + h(X)[0]**2 + h(X)[1]**2 + h(X)[2]**2
+
+def B(X, r):
+    if g(X) == 0 and h(x) <= 0:
+        return f(X)
+    else:
+        return f(X) + 1/r * b(X)
+
+print (f"Baudos test: {B(X1, 0.2)}")   
